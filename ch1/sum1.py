01b84b6d8e0c5fb12e29a47d0382a18da9c424a0
Kwang Hyuk Ko <42025932+k2hcis03@users.noreply.github.com>
2023-12-14 23:53:36 +0900

    Update sum1.py

--- ch1/sum1.py
+++ ch1/sum1.py
@@ -1,7 +1,7 @@
 def main():  
   x = int(input("첫번째 입력 :: "))  
   y = int(input("두번째 입력 :: "))  
-  print("두 입력 값 {} 와 {}의 합은 {}입니다".format(x,y,x+y))  
+  print("f두 입력 값 {x} 와 {y}의 합은 {x+y}입니다")  
   
 if __name__ == "__main__":  
   main() 
