c63a91a5a897d794be36bfb1a477e133dac547f4
Kwang Hyuk Ko <42025932+k2hcis03@users.noreply.github.com>
2023-12-21 19:36:40 +0900

    Update recipe2.c

--- part2/recipe2/recipe2.c
+++ part2/recipe2/recipe2.c
@@ -1,1 +1,30 @@
+/*
+ * my_first_module.c
+ *
+ *  Created on: 2023. 12. 18.
+ *      Author: k2h
+ */
 
+#include <linux/module.h>
+
+static int param = 100;
+module_param(param, int, S_IRUGO);
+
+static int __init hello_init(void)
+{
+	pr_info("Hello world init\n");
+	pr_info("parameter is %d\n", param);
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Hello world exit\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kwang Hyuk Ko");
+MODULE_DESCRIPTION("This is a simple init/exit module test");
