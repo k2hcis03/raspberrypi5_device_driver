95620db9331c5eb6ede2c4156132de8ddd353e33
Kwang Hyuk Ko <42025932+k2hcis03@users.noreply.github.com>
2023-12-22 10:18:23 +0900

    Update recipe3.py

--- part2/recipe3/recipe3.py
+++ part2/recipe3/recipe3.py
@@ -1,1 +1,16 @@
+import os
 
+def main():
+    try:
+        dev = os.open("/dev/recipedev", os.O_RDWR)
+        data = bytes([ord('A'),ord('B'),ord('C'),ord('D')])
+        os.write(dev,data)
+        cnt = os.read(dev, 8)
+        print(cnt)
+        os.close(dev)
+    except OSError as e:
+        print(e.errno, "Fail to open device file: /dev/recipedev.")
+        print(e.strerror)
+
+if __name__ == "__main__":
+    main()
