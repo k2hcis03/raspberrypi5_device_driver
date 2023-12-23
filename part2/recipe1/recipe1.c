acebbf79f57dbba2fee283b9b65e9d71b224f2d8
Kwang Hyuk Ko <42025932+k2hcis03@users.noreply.github.com>
2023-12-23 22:49:16 +0900

    Update recipe1.c

--- part2/recipe1/recipe1.c
+++ part2/recipe1/recipe1.c
@@ -1,1 +1,27 @@
+/*
+ * recipe1.c
+ *
+ *  Created on: 2021. 1. 18.
+ *      Author: k2h
+ */
+
+#include <linux/module.h>
+
+static int __init recipe_init(void)
+{
+	pr_info("Hello world init\n");
+	return 0;
+}
+
+static void __exit recipe_exit(void)
+{
+	pr_info("Hello world exit\n");
+}
+
+module_init(recipe_init);
+module_exit(recipe_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kwang Hyuk Ko");
+MODULE_DESCRIPTION("This is a simple init/exit module test");
 
