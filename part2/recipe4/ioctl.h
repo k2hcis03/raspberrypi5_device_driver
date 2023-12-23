e3b06f9228bd422360fbab54afabf7d1f5902af9
Kwang Hyuk Ko <42025932+k2hcis03@users.noreply.github.com>
2023-12-23 21:56:09 +0900

    Update ioctl.h

--- part2/recipe4/ioctl.h
+++ part2/recipe4/ioctl.h
@@ -1,1 +1,14 @@
+#ifndef IOCTL_H_
+#define IOCTL_H_
+
+struct __attribute__((packed)) ioctl_info {
+       uint32_t size;
+       uint32_t buffer[64];
+};
+
+#define             IOCTL_MAGIC         'R'
+#define             SET_DATA            _IOW(IOCTL_MAGIC, 2, struct ioctl_info )
+#define             GET_DATA            _IOR(IOCTL_MAGIC, 3, struct ioctl_info )
+
+#endif /* IOCTL_H_ */
 
