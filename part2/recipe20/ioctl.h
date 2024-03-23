7586680d6646c31f74813f70512bc670bbd6c467
Kwang Hyuk Ko <42025932+k2hcis03@users.noreply.github.com>
2024-03-23 12:21:25 +0900

    Create ioctl.h

---
+++ part2/recipe20/ioctl.h
@@ -0,0 +1,13 @@
+#ifndef IOCTL_H_
+#define IOCTL_H_
+
+struct __attribute__((packed)) ioctl_info {
+	int pwm_on_time_ns;
+	int pwm_period_ns;
+};
+
+#define             IOCTL_MAGIC				'R'
+#define             SET_DATA            	_IOW(IOCTL_MAGIC, 2, struct ioctl_info )
+#define             GET_DATA         		_IOR(IOCTL_MAGIC, 3, struct ioctl_info )
+
+#endif /* IOCTL_H_ */
